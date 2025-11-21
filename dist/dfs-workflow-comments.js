91e438dbf2251563c48b86922bb5892a90346545
BradPage <bradpage_@hotmail.com>
2025-11-21 14:32:51 +0000

    fix: update button label from 'Add Comment' to 'Click to Post Comment to Workflow'

--- dist/dfs-workflow-comments.js
+++ dist/dfs-workflow-comments.js
@@ -277,7 +277,7 @@ svg></svg>
             @click=${this.addComment}
             ?disabled=${!this.newComment.trim()}
           >
-            ${it} Add Comment
+            ${it} Click to Post Comment to Workflow
           </button>
         </div>
       `}
